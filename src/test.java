2a0c1f176cc06a8a551477f44b5401fb81cdcae0
Sebastian Quintero <sebastianjaimesq@gmail.com>
2024-12-11 23:45:16 -0500

    TDD Added

--- src/test.java
+++ src/test.java
@@ -1,26 +1,41 @@
-import Operaciones.*; 
-import java.io.*;
+import Operaciones.*;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;;
+
 public class test {
-    
+
     Operacion suma = new Suma();
     Operacion resta = new Resta();
     Operacion multiplicacion = new Multiplicar();
-    Operacion dividir = new Dividir();    
-    Operacion[] operaciones = {suma, resta, multiplicacion, dividir};
-    
-    
-    public void sample() {
+    Operacion dividir = new Dividir();
+    Operacion[] operaciones = { suma, resta, multiplicacion, dividir };
 
-        for (Operacion operator : operaciones) {
-            System.out.println(operator.operar(10, 5));
-            System.out.println(operator.operar(10.5f, 5.5f));
-            System.out.println(operator.operar(10.5, 5.5));
-            
-        }
+    @Test
+    public void testOperaciones() {
+        assertEquals((10.5 + 5.5), suma.operar(10.5, 5.5), 0);
+        assertEquals((10.5 - 5.5), resta.operar(10.5, 5.5), 0);
+        assertEquals((10.5 * 5.5), multiplicacion.operar(10.5, 5.5), 0);
+        assertEquals((10.5 / 5.5), dividir.operar(10.5, 5.5), 0);
     }
-    public static void main(String[] args) {
-        test lol = new test();
-        lol.sample();
 
-    }
+    // Codigo Muerto
+    /*
+     * public void sample() {
+     * 
+     * for (Operacion operator : operaciones) {
+     * System.out.println(operator.operar(10, 5));
+     * System.out.println(operator.operar(10.5f, 5.5f));
+     * System.out.println(operator.operar(10.5, 5.5));
+     * 
+     * }
+     * }
+     * 
+     * public static void main(String[] args) {
+     * test lol = new test();
+     * lol.sample();
+     * 
+     * }
+     */
 }
